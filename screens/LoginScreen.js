21991695db415fd03d4a47e4bf41e007c1d6e75e
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-28 15:08:25 -0500

    authentication error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,19 @@ import AuthContent from '../components/Auth/AuthContent';
 import { useState } from 'react';
 import { login } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({email, password}) {
     setIsAuthenticating(true);
-   await login(email, password);
+    try{
+      await login(email, password);
+    } catch (error) {
+      Alert.alert('Authentication Failed!', 'Could not log you in. Please check your credentials and try again.');
+    }
+   
     setIsAuthenticating(false);
   }
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,13 +2,19 @@ import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function signupHandler({email, password}) {
     setIsAuthenticating(true);
-   await createUser(email, password);
+    try{
+      await createUser(email, password);
+    }catch(error){
+      Alert.alert('Authentication Failed!', 'Could not create a user. Please try again later.');
+    }
+   
     setIsAuthenticating(false);
   }
 
